8b91f95d091e28bbf0865e81273d38357f779baf
roshane <roshanemadusanka@gmail.com>
2018-03-09 22:41:53 +0930

    - added maven shade plugin - fixed log4j2 issue with shade plugin

--- src/main/java/com/aeon/api/ApiBoot.java
+++ src/main/java/com/aeon/api/ApiBoot.java
@@ -1,30 +1,43 @@
 package com.aeon.api;
 
 import com.aeon.api.resource.ApplicationConfig;
+import com.aeon.api.resource.CustomerResource;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Collections;
+import java.util.Optional;
 
 /**
  * Created by roshane on 11/9/2017.
  */
 public class ApiBoot {
 
-    private static final URI BASE_URI = URI.create("http://localhost:8080/api/");
-
-    private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(ApiBoot.class);
+    private static final Logger logger = LogManager.getLogger(ApiBoot.class);
 
     public static void main(String[] args) {
+
+        String serverPort = Optional.ofNullable(System.getProperty("server.port"))
+                .orElse("8080");
+
+        System.setProperty("server.port", serverPort);
+
+        URI BASE_URI = URI.create(String.format("http://localhost:%s/api/", serverPort));
+
+        addShutdownHook();
+
         try {
             logger.debug("application starting");
             System.out.println("JSON with Jackson Jersey Example App");
 
             ResourceConfig configuration = createApp();
             final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, configuration, false);
+
             server
                     .getServerConfiguration().getHttpHandlersWithMapping()
                     .entrySet().forEach(e -> {
@@ -44,7 +57,15 @@ public class ApiBoot {
 
     }
 
+    static void addShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.debug(">>> shutting down the service");
+        }));
+    }
+
     static ResourceConfig createApp() {
-        return new ApplicationConfig();
+        ApplicationConfig config = new ApplicationConfig();
+        config.registerClasses(Collections.singleton(CustomerResource.class));
+        return config;
     }
 }
--- src/main/java/com/aeon/api/resource/CustomerResource.java
+++ src/main/java/com/aeon/api/resource/CustomerResource.java
@@ -1,5 +1,8 @@
 package com.aeon.api.resource;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,10 +17,23 @@ import java.util.Map;
 @Path("hello")
 public class CustomerResource {
 
+    private static final Logger logger = LogManager.getLogger("APP");
+    private static final String SERVER_PORT = System.getProperty("server.port");
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> apiInfo() throws Exception {
+//        throw new Exception("internal server error");
+        logger.debug("hit for server [{}]", SERVER_PORT);
+        return Collections.singletonMap("message", "api version V1.0.0");
+    }
+
     @GET
     @Path("/1")
     @Produces(MediaType.APPLICATION_JSON)
     public Map<String, LocalDateTime> getTime() {
+        logger.debug("hit for server [{}]", SERVER_PORT);
         return Collections.singletonMap("dateTime", LocalDateTime.now());
     }
 }
